223ea1d30218831e5b927e3acc1a7bdefc85cda4
jokercom <jokercom@aazz.kr>
2021-12-29 11:32:15 +0900

    a

---
+++ test/FirstProject/FirstProject/first.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#define PII 3.14
+#pragma warning(disable : 4996)
+int main()
+{
+    printf("우기석\n");
+
+    //long long aa = 21474836484567L;
+    //printf("넓이 : %lld\n", aa); // 계산값 출력
+    //printf("넓이 : %d\n", (unsigned)2147483647+1); // 계산값 출력
+
+    return 0;
+}
